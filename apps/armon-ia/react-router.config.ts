ad709c9964129405d965a875d5805ae353ae6431
AndresLGomezO <36666046+AndresLGomezO@users.noreply.github.com>
2025-09-22 18:15:07 -0500

    Add eslint and prettier (#4)
    * Add eslint and prettier
    
    * Update pnpm install command
    
    * Update format command
    
    * Add eslint
    
    * Add typecheck to the pipeline
    
    * Split the CI logic into independent jobs
    
    ---------
    
    Co-authored-by: Andrés Gómez Ortiz <andresgomezortiz@192.168.2.5>

--- apps/armon-ia/react-router.config.ts
+++ apps/armon-ia/react-router.config.ts
@@ -1,9 +1,9 @@
-import type { Config } from "@react-router/dev/config";
+import type { Config } from '@react-router/dev/config'
 
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
   basename: process.env.NODE_ENV === 'production' ? '/armon-ia/' : '/',
-  buildDirectory: 'build'
-} satisfies Config;
+  buildDirectory: 'build',
+} satisfies Config
