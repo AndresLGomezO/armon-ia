7648dc65af9d2b50078814d44682a8aa9f5aba71
AndresLGomezO <36666046+AndresLGomezO@users.noreply.github.com>
2025-09-25 07:27:58 -0500

    Text a simple change to see vercel preview (#7)
    * feat:add vercel as preview system
    
    * fix:Fix formats and lint
    
    ---------
    
    Co-authored-by: Andrés Gómez Ortiz <andresgomezortiz@192.168.2.5>

--- apps/armon-ia/react-router.config.ts
+++ apps/armon-ia/react-router.config.ts
@@ -1,9 +1,16 @@
 import type { Config } from '@react-router/dev/config'
 
+const isVercel = process.env.VERCEL === '1'
+const basename = isVercel
+  ? '/'
+  : process.env.NODE_ENV === 'production'
+    ? '/armon-ia/'
+    : '/'
+
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
-  basename: process.env.NODE_ENV === 'production' ? '/armon-ia/' : '/',
+  basename,
   buildDirectory: 'build',
 } satisfies Config
---
+++ apps/armon-ia/server/app.ts
@@ -0,0 +1,28 @@
+import { createRequestHandler } from '@react-router/express'
+import express from 'express'
+import 'react-router'
+
+export const app = express()
+
+const reactRouterHandler = createRequestHandler({
+  // Pulls the built module from Vite
+  build: () => import('virtual:react-router/server-build'),
+  getLoadContext() {
+    // context values to pass through to React-Router
+    return {
+      VALUE_FROM_EXPRESS: 'Hello from Express',
+    }
+  },
+})
+
+app.use(async (req, res, _next) => {
+  try {
+    try {
+      await reactRouterHandler(req, res, _next)
+    } catch (routerError) {
+      throw routerError
+    }
+  } catch (error) {
+    throw error
+  }
+})
